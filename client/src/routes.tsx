6a4d5e9d94738da016032c52e512b92a7548baf5
mohammedelgammal <mohammedelgammalamazon@gmail.com>
2024-03-21 18:06:13 +0200

    update_baseName_router

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -6,25 +6,28 @@ import Games from "./pages/HomeScreen";
 import Register from "./pages/RegisterScreen";
 import Login from "./pages/LoginScreen";
 
-export default createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      { index: true, element: <Games /> },
-      {
-        path: "games/:id",
-        element: <GameDetails />,
-      },
-      {
-        path: "register",
-        element: <Register />,
-      },
-      {
-        path: "login",
-        element: <Login />,
-      },
-    ],
-  },
-]);
+export default createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        { index: true, element: <Games /> },
+        {
+          path: "games/:id",
+          element: <GameDetails />,
+        },
+        {
+          path: "register",
+          element: <Register />,
+        },
+        {
+          path: "login",
+          element: <Login />,
+        },
+      ],
+    },
+  ],
+  { basename: "/g_atlas" }
+);
